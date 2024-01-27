81b65f1bf24914c90441978cece5983748454f5e
telkontardev <telkontardev@gmail.com>
2024-01-27 14:54:03 +0100

    removed max width

--- components/richText/RichTextBlocks.vue
+++ components/richText/RichTextBlocks.vue
@@ -47,7 +47,7 @@ function renderText(children: RichTextBlockChild[]) {
 </script>
 
 <template>
-  <div class="prose prose-slate prose-a:text-blue-600">
+  <div class="prose prose-slate prose-a:text-blue-600 max-w-none">
     <template v-for="(block, index) in data" :key="index">
       <!-- Paragraph -->
       <p v-if="block && block.type === RichTextBlockType.paragraph">
