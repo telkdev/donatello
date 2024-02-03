af1a58d6ad6d57d86d5072b2dc320a596e66d785
telkontardev <telkontardev@gmail.com>
2024-02-03 16:20:07 +0100

    domain structure init

---
+++ domain/Fund.ts
@@ -0,0 +1,17 @@
+// TODO: add me
+// export type Fund = {
+//   id: number;
+//   image: {
+//     data: WithStrapiStructure<Media>;
+//   };
+//   title: string;
+//   slug: string;
+//   description: RichTextBlocks[];
+//   createdAt: string;
+//   totalGoal: number;
+//   collectedAmount: number;
+//   requisites: StrapiRequisite;
+//   documents: StrapiDocuments;
+//   category: StrapiCategory;
+//   organization: StrapiOrganization;
+// };
