5295296a8e9f1e751257379c5ae428639147a5c0
telkontardev <telkontardev@gmail.com>
2024-03-09 12:32:18 +0100

    add computed locales

--- components/NavigationSecondary.vue
+++ components/NavigationSecondary.vue
@@ -3,12 +3,14 @@
     <div class="container py-5 md:py-6">
       <ul class="flex items-center flex-wrap">
         <li v-for="(nav, index) in navigation" :index="index">
-          <NuxtLink
-            :to="nav.link"
-            class="text-xl md:text-2xl"
-            >{{ nav.name }}</NuxtLink
+          <NuxtLink :to="nav.link" class="text-xl md:text-2xl">{{
+            nav.name
+          }}</NuxtLink>
+          <span
+            v-if="index < navigation.length - 1"
+            class="mx-4 text-xl md:text-2xl"
+            >/</span
           >
-          <span v-if="index < navigation.length - 1" class="mx-4 text-xl md:text-2xl">/</span>
         </li>
       </ul>
     </div>
@@ -20,16 +22,16 @@ import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
 
-const navigation = [
+const navigation = computed(() => [
   {
-    name: t('Main.ActionLinks.Funds'),
+    name: t("ActionLinks.Funds"),
     link: "/funds",
   },
   {
-    name: t('Main.ActionLinks.CreateFundraiser'),
+    name: t("ActionLinks.CreateFundraiser"),
     link: `${process.env.SERVER_URL}/admin/auth/register`,
   },
-];
+]);
 </script>
 
 <style scoped>
